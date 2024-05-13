5a69f15bc9b414fd0addfcb142848d16de8d7108
Hamza Amin <mh4070685@gmail.com>
2024-05-13 23:40:55 +0500

    Revived review system

--- database.py
+++ database.py
@@ -515,7 +515,8 @@ class Database:
         ON ORDERED.pay_id = PAYMENT.pay_id
         INNER JOIN FOOD
         ON ORDERED.food_no = FOOD.food_no
-        where CUSTOMERS.customer_id = '{customerId}' AND ORDERED.order_status = 'Issued'
+        where CUSTOMERS.customer_id = '{customerId}' AND (ORDERED.order_status = 'Issued' OR ORDERED.order_status = 'Pending' 
+                                                                                         OR ORDERED.order_status = 'Delivered')
         order by ORDERED.Order_ID DESC
         ''')
         return orderDetails
